52141cd9b48ca886f80eba7602b676465e8c1183
Cristina Lare <100524474+cristinalare@users.noreply.github.com>
2024-10-05 01:07:17 +0300

    add basepath

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   // output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? 'https://allo.expert/' : 'http://localhost:3000',
   images: {
     domains: ['res.cloudinary.com'],
     remotePatterns: [
