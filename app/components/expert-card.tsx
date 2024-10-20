47e8a723d5b23389cad0f582d8c47449dc9e9c22
Cristina Lare <100524474+cristinalare@users.noreply.github.com>
2024-10-21 00:40:29 +0300

    improve expert card layout

--- app/components/expert-card.tsx
+++ app/components/expert-card.tsx
@@ -6,31 +6,32 @@ import Link from "next/link";
 export default function ExpertCard({ expert }: { expert: Expert }) {
   return (
     <>
-      <div className="flex flex-col gap-4">
-        <Link href={`/experts/${expert.slug}`}>
-          <Image
-            src={`${process.env.NEXT_PUBLIC_CLOUDINARY_BASE_URL}/${expert.avatar}`}
-            alt=""
-            className={`rounded-[32px] border-2 border-green-700 sm:w-[250px] sm:h-[250px] aspect-square`}
-            width={250}
-            height={250}
-          />
-        </Link>
-
-        <div>
+      <div className="flex flex-col gap-4 justify-between">
+        <div className="flex flex-col gap-4">
           <Link href={`/experts/${expert.slug}`}>
-            <h3 className="font-bold text-xl sm:text-2xl text-blue-600 mb-1">
-              {expert.name}
-            </h3>
+            <Image
+              src={`${process.env.NEXT_PUBLIC_CLOUDINARY_BASE_URL}/${expert.avatar}`}
+              alt=""
+              className={`rounded-[32px] border-2 border-green-700 sm:w-[250px] sm:h-[250px] aspect-square`}
+              width={250}
+              height={250}
+            />
           </Link>
-          <p className="sm:text-base text-sm">
-            Expert in{" "}
-            {expert.expert_in == EXPERT_IN.ALLO_DEV
-              ? "Allo Dev"
-              : "Allo Mechanisms"}
-          </p>
-        </div>
 
+          <div>
+            <Link href={`/experts/${expert.slug}`}>
+              <h3 className="font-bold text-xl sm:text-2xl text-blue-600 mb-1">
+                {expert.name}
+              </h3>
+            </Link>
+            <p className="sm:text-base text-sm">
+              Expert in{" "}
+              {expert.expert_in == EXPERT_IN.ALLO_DEV
+                ? "Allo Dev"
+                : "Allo Mechanisms"}
+            </p>
+          </div>
+        </div>
         <ContactButton expert={expert} />
       </div>
     </>
