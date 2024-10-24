dcdd7f79083b73277b5ad80113f0bec5145ba3e2
Cristina Lare <100524474+cristinalare@users.noreply.github.com>
2024-10-24 17:45:25 +0300

    feat: render single line breaks

--- app/components/render-markdown.tsx
+++ app/components/render-markdown.tsx
@@ -1,6 +1,11 @@
 import styles from "../markdown.module.css";
 import { marked } from "marked";
 
+marked.setOptions({
+  gfm: true, // Enable GitHub Flavored Markdown
+  breaks: true, // Convert line breaks into <br> tags
+});
+
 async function formatMarkdown(data: string) {
   const contentHtml = await marked.parse(data);
   return contentHtml;
